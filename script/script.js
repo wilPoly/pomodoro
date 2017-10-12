c44d99ce6866b04e6ebc9bd25e05b5fd57189d12
wilPoly <misterwrinkle@gmail.com>
2017-10-12 22:58:10 +0200

    Back to bare html to work on core functions. Began to structure time display

--- script/script.js
+++ script/script.js
@@ -1,7 +1,39 @@
-function timer() {
-    
+function timer(time){
+	var seconds = time * 60
+	var minutes = seconds / 60;
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	if (seconds == seconds - 1){
+
+	}
+	var display = minutes + ":" + seconds;
+
+	$("#timer").text(display);
+
+	$("#play").click('button', function(){
+		setInterval(function(){
+			seconds = seconds - 1;
+			$("#timer").text(display);
+			console.log(seconds);
+		}, 1000);
+	})
 }
 
-function second(){
-    
+
+function decrement(){
+	seconds = seconds - 1;
+	$("#timer").text(display);
+
 }
+
+
+$(document).ready(function(){
+	timer(25);
+	
+})
