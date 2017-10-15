956a0c62135934a6d7962881dbcea4282493e080
wilPoly <misterwrinkle@gmail.com>
2017-10-15 23:16:39 +0200

    Time display and decrement function implemented

--- script/script.js
+++ script/script.js
@@ -1,39 +1,37 @@
-function timer(time){
-	var seconds = time * 60
-	var minutes = seconds / 60;
-	seconds = seconds % 60;
+$(document).ready(function(){
+var seconds = 1500;
+var display;
+var count;
+$("#timer").text(timer(seconds));
+
+function timer(seconds) {
+	var minutes = Math.floor(seconds / 60);
+	seconds = Math.floor(seconds % 60);
+
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-
-	if (seconds == seconds - 1){
-
-	}
-	var display = minutes + ":" + seconds;
-
-	$("#timer").text(display);
-
-	$("#play").click('button', function(){
-		setInterval(function(){
-			seconds = seconds - 1;
-			$("#timer").text(display);
-			console.log(seconds);
-		}, 1000);
-	})
-}
-
-
-function decrement(){
-	seconds = seconds - 1;
-	$("#timer").text(display);
-
-}
-
-
-$(document).ready(function(){
-	timer(25);
 	
-})
+	display = minutes + ":" + seconds;
+	
+	return display;
+}
+
+function decrement() {
+	seconds = seconds - 1;
+	if (seconds === 0) {
+		clearInterval(count);
+	}
+	$("#timer").text(timer(seconds));
+}
+
+function counter() {
+	count = setInterval(decrement, 1000);
+}
+
+	$("#play").click('button', counter);
+	
+});
