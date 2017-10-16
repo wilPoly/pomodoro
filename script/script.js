757243ef7cfb0b71cf40541935f1ec441d51dafd
wilPoly <misterwrinkle@gmail.com>
2017-10-17 00:03:00 +0200

    Added end session sound, working on switching from work to break mode

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-var seconds = 1500;
+var workTime = 5;
+var seconds = workTime;
+var breakTime = 300;
 var display;
 var count;
+var workMode = true;
 var play = false;
+var sound = new Audio("./media/salt_machine.mp3")
 $("#timer").text(timer(seconds));
 
 function timer(seconds) {
@@ -22,20 +26,30 @@ function timer(seconds) {
 }
 
 function decrement() {
-	seconds = seconds - 1;
 	if (seconds === 0) {
-		clearInterval(count);
+		//clearInterval(count);
+		sound.play();
+			if (workMode === true) {
+				seconds = workTime;
+				workMode = false;
+			}
+			else {
+				seconds = breakTime;
+				workMode = true;
+			}
+		$("#timer").text(timer(seconds));
+	}		
+	else {
+		seconds = seconds - 1;
+		$("#timer").text(timer(seconds));
 	}
-	$("#timer").text(timer(seconds));
 }
 
 function counter() {
 	count = setInterval(decrement, 1000);
 }
 
-// function playPause() {
-
-	$("#play").click('button', function(){
+function playPause() {
 		if (play === false){
 			counter();
 			play = true;
@@ -46,10 +60,21 @@ function counter() {
 			play = false;
 			$("#play").text('Play');
 		}
+}
 
-	});
-// }
+function reset () {
+	clearInterval(count);
+	seconds = workTime;
+	$("#timer").text(timer(seconds));
+	play = false;
+	$("#play").text('Play');
+}
 
-	
+
+// Controls
+$("#play").click('button', playPause);
+$("#reset").click('button', reset);
+
+
 	
 });
